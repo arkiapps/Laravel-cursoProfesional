6d44ff1f5b8d1b23e6d56cda58332566f0ca715c
melomatthewdev <devscustomers@outlook.com>
2024-10-21 07:20:08 -0500

    web.php, about.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
+
 
 // Metodos estaticos
-Route::view('/', 'welcome')->name('welcome');
+Route::view('/', 'landing.about')->name('about');
 //Route.view('/', 'welcome').name('welcome');
 
 // Route::view(); //
