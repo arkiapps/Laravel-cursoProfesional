7ea8f87cb4e00e5ec8e984180850e72312d9d98a
melomatthewdev <devscustomers@outlook.com>
2025-02-03 09:45:48 -0500

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-// Metodos estaticos
-Route::view('/', 'landing.index')->name('index');
-//Route.view('/', 'welcome').name('welcome');
-Route::view('/about', 'landing.about')->name('about');
 
 // Route::view(); //
 // Route::get('mi/ruta', ControlladorDeLaRuta);
